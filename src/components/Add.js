a0fffad142f62706f1213442926dadd89d0cc341
Lucas Barroso <lucasbarroso23@gmail.com>
2020-09-23 15:05:58 -0300

    configurando chamada de api para a pagina add

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Add = () => {
+
+    const [query, setQuery] = useState("");
+
+    const onChange = (e) => {
+        e.preventDefault();
+
+        setQuery(e.target.value);
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }
+
     return (
-        <div>
-            <h1>Add Page</h1>
+        <div className="add-page">
+            <div className="container">
+                <div className="add-content">
+                    <div className="input-wrapper">
+                        <input
+                            type="text"
+                            placeholder="Search for a Movie"
+                            value={query}
+                            onChange={onChange}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
